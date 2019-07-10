44be3adbd8592535959571e80927b83ae9368696
Ana Nikolova <ani.nik19@gmail.com>
2019-07-10 06:49:44 +0200

    got the jokes from the api

--- Joke/src/JokeList.js
+++ Joke/src/JokeList.js
@@ -5,13 +5,40 @@ import uuid from "uuid/v4";
 import "./JokeList.css";
 
 class JokeList extends Component {
+  static defaultProps = {
+    jokesToGet: 10
+  };
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      jokes: []
+    };
+  }
+  async componentDidMount() {
+    let jokes = [];
+    while (jokes.length < this.props.jokesToGet) {
+      const res = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" }
+      });
+      jokes.push(res.data.joke);
+    }
+    this.setState({
+      jokes: jokes
+    });
+    console.log(jokes);
   }
 
   render() {
-    return <div className="JokeList">JokeList</div>;
+    return (
+      <div className="JokeList">
+        <h1>Dad jokes</h1>
+        <div className="JokeList-joke">
+          {this.state.jokes.map(j => (
+            <div>{j}</div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 export default JokeList;
