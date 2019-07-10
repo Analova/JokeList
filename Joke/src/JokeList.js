c1338b622bb173ac4f74e2bfe378922af8051344
Ana Nikolova <ani.nik19@gmail.com>
2019-07-10 07:11:49 +0200

    styling the JokeList

--- Joke/src/JokeList.js
+++ Joke/src/JokeList.js
@@ -31,7 +31,17 @@ class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad </span>jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt=""
+          />
+          <button className="JokeList-btn">New Jokes</button>
+        </div>
+
         <div className="JokeList-joke">
           {this.state.jokes.map(j => (
             <div>{j}</div>
